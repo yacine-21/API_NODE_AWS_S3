27a52fc15f1ba11fb5d0de3531999209d35a3270
yacine-21 <yacinelyoubi37@gmail.com>
2022-06-26 15:59:49 +0200

    add get a folder content

--- index.js
+++ index.js
@@ -10,29 +10,49 @@ const s3 = new AWS.S3({
 
 const BUCKET_NAME = process.env.BUCKET_NAME;
 let images = [];
+let images_folder_carousel = [];
 
 const listBuckets = async s3 => {
 	s3.listBuckets(function (err, data) {
 		if (err) {
 			console.log("Error", err);
 		} else {
-			console.log("Bucket List", data.Buckets);
+			// console.log("Bucket List", data.Buckets);
 		}
 	});
 };
 
+const listObjectFolder = async s3 => {
+	s3
+		.listObjectsV2({ Bucket: BUCKET_NAME, Prefix: "Carousel" }, (err, data) => {
+			if (err) {
+				console.log("Error", err);
+			} else {
+				data.Contents.map(item => {
+					images_folder_carousel.push(item.Key);
+				});
+				console.log(data);
+			}
+		})
+		.promise();
+};
+
 const listObjectsInBucket = async bucketName => {
 	let bucketParams = {
-		Bucket: bucketName
+		Bucket: bucketName,
+		Prefix: "/Images"
 	};
 	s3.listObjects(bucketParams, function (err, data) {
-		if (err) {
-			console.log("Error", err);
-		} else {
-			data.Contents.map(item => {
-				console.log(`https://s3.amazonaws.com/${bucketName}/${item.Key}`);
-				images.push(`https://s3.amazonaws.com/${bucketName}/${item.Key}`);
-			});
+		try {
+			if (err) {
+				console.log("Error", err);
+			} else {
+				data.Contents.map(item => {
+					images.push(`https://s3.amazonaws.com/${bucketName}/${item.Key}`);
+				});
+			}
+		} catch (error) {
+			console.log(error);
 		}
 	});
 };
@@ -40,6 +60,7 @@ const listObjectsInBucket = async bucketName => {
 async function main() {
 	await listBuckets(s3);
 	await listObjectsInBucket(BUCKET_NAME);
+	await listObjectFolder(s3);
 }
 
 main();
@@ -50,6 +71,10 @@ app.get("/", (req, res) => {
 
 app.get("/get-all-images", (req, res) => {
 	res.send(images);
+});
+
+app.get("/get-folder-carousel", (req, res) => {
+	res.send(images_folder_carousel);
 });
 
 app.listen(PORT, () => {
